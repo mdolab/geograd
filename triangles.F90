bc23152a98e4e6300907ed6c02db9d78d76b6709
Benjamin Brelje <benjamin.brelje@gmail.com>
2019-11-25 23:34:00 -0500

    Close point comp for points and triangles

--- triangles.F90
+++ triangles.F90
@@ -7,9 +7,9 @@ module triangles
         real, dimension(3), intent(in) :: a, b, c, p
         real, dimension(3), intent(out) :: d
 
-        real, dimension(3) :: ab, ac, ap, bp
-        real :: d1, d2, d3, d4
-        real :: v, vc
+        real, dimension(3) :: ab, ac, ap, bp, cp
+        real :: d1, d2, d3, d4, d5, d6
+        real :: v, vc, vb, va, w, denom
 
         ab = b - a
         ac = c - a
@@ -34,10 +34,42 @@ module triangles
         vc = d1*d4 - d3*d2
         if (vc <= 0.0 .AND. d1 >= 0.0 .AND. d3 <= 0.0) then
             v = d1 / (d1 - d3)
-            d = a + v * ab
+            d = a + v * ab ! barycentric coordinates (1-v,v,0)
             return 
         end if
+        
+        ! Check if P in vertex region C
+        cp = p - c
+        d5 = dot_product(ab, cp)
+        d6 = dot_product(ac, cp)
+        if (d6 >= 0.0 .AND. d5 <= d6) then
+            d = c ! barycentric coordinates (0,0,1)
+            return
+        end if
 
+        ! check if P in edge region of AC, if so, return proj(P,AC)
+        vb = d5*d2 - d1*d6
+        if (vb <= 0.0 .AND. d2 >= 0.0 .AND. d6 <=0.0) then
+            w = d2 / (d2 - d6)
+            d = a + w * ac ! barycentric (1-w, 0, w)
+            return
+        end if
+
+        ! Check if P in edge region of BC, if so, return proj(P,BC)
+        va = d3*d6 - d5*d4
+        if (va <= 0.0 .AND. (d4-d3) >= 0.0 .AND. (d5-d6) >= 0.0) then
+            w = (d4 - d3) / ((d4 - d3) + (d5 - d6))
+            d = b + w * (c - b) ! barycentric (0, 1-w, w)
+            return
+        end if
+
+        ! P inside face region. Compute Q through barycentric (u, v, w)
+        denom = 1.0 / (va + vb + vc)
+        v = vb * denom
+        w = vc * denom
+        d = a + ab * v + ac * w
+        return
+        
     end subroutine point_tri
 
 end module triangles
