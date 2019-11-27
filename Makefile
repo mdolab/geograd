ed660346ba3937f421b46cfada266dc38a169c2e
Benjamin Brelje <benjamin.brelje@gmail.com>
2019-11-27 08:05:41 -0500

    Double precision, module split

--- Makefile
+++ Makefile
@@ -4,12 +4,20 @@
 default: triangles.F90
 	python -m numpy.f2py -c triangles.F90 -m triangles
 
-python3: triangles.F90
-	python3 -m numpy.f2py -c triangles_db.pyf triangles_db.f90
+python3: triangles_db.f90 triangles.pyf
+	python3 -m numpy.f2py -c triangles.pyf triangles_db.f90 triangles.F90
+
+pyf: triangles_db.f90
+	python3 -m numpy.f2py triangles_db.f90 triangles.F90 -m triangles -h triangles.pyf
+
+tapenade: triangles.F90
+	tapenade triangles.F90 -d -b -root point_tri
 
 clean:
 	rm *.mod -f
 	rm *.so -f 
+	rm *.msg~ -f
+	rm *.f90~ -f
 
 test:
 	python3 test.py
--- triangles.F90
+++ triangles.F90
@@ -1,30 +1,34 @@
 module triangles
     implicit none
+    real(kind=8), private :: dummyreal
+    integer, parameter :: realType = kind(dummyreal)
+    real(kind=realType) :: one = 1.0
+    real(kind=realType) :: zero = 0.0
     contains
 
     subroutine dot_prod(d, v, w)
         implicit none
-        real, dimension(3), intent(in) :: v, w
-        real, intent(out) :: d
+        real(kind=realType), dimension(3), intent(in) :: v, w
+        real(kind=realType), intent(out) :: d
         d = v(1)*w(1)+v(2)*w(2)+v(3)*w(3)
     end subroutine dot_prod
 
 
     subroutine point_tri(a, b, c, p, dsquared)
         implicit none
-        real, dimension(3), intent(in) :: a, b, c, p
-        real, intent(out) :: dsquared
+        real(kind=realType), dimension(3), intent(in) :: a, b, c, p
+        real(kind=realType), intent(out) :: dsquared
 
-        real, dimension(3) :: ab, ac, ap, bp, cp, closepoint, diff, dummydiff
-        real :: d1, d2, d3, d4, d5, d6
-        real :: v, vc, vb, va, w, denom
+        real(kind=realType), dimension(3) :: ab, ac, ap, bp, cp, closepoint, diff, dummydiff
+        real(kind=realType) :: d1, d2, d3, d4, d5, d6
+        real(kind=realType) :: v, vc, vb, va, w, denom
 
         ab = b - a
         ac = c - a
         ap = p - a
         call dot_prod(d1, ab, ap)
         call dot_prod(d2, ac, ap)
-        if (d1 <= 0.0 .AND. d2 <= 0.0) then
+        if (d1 <= zero .AND. d2 <= zero) then
             closepoint = a  ! barycentric 1, 0, 0
             diff = closepoint - p
             dummydiff = diff
@@ -36,7 +40,7 @@ module triangles
         bp = p - b
         call dot_prod(d3, ab, bp)
         call dot_prod(d4, ac, bp)
-        if (d3 >= 0.0 .AND. d4 <= d3) then
+        if (d3 >= zero .AND. d4 <= d3) then
             closepoint = b ! barycentric 0, 1, 0
             diff = closepoint - p
             dummydiff = diff
@@ -46,7 +50,7 @@ module triangles
 
         ! check if P in edge region of AB, if so return projection of P onto AB
         vc = d1*d4 - d3*d2
-        if (vc <= 0.0 .AND. d1 >= 0.0 .AND. d3 <= 0.0) then
+        if (vc <= zero .AND. d1 >= zero .AND. d3 <= zero) then
             v = d1 / (d1 - d3)
             closepoint = a + v * ab ! barycentric coordinates (1-v,v,0)
             diff = closepoint - p
@@ -59,7 +63,7 @@ module triangles
         cp = p - c
         call dot_prod(d5, ab, cp)
         call dot_prod(d6, ac, cp)
-        if (d6 >= 0.0 .AND. d5 <= d6) then
+        if (d6 >= zero .AND. d5 <= d6) then
             closepoint = c ! barycentric coordinates (0,0,1)
             diff = closepoint - p
             dummydiff = diff
@@ -69,7 +73,7 @@ module triangles
 
         ! check if P in edge region of AC, if so, return proj(P,AC)
         vb = d5*d2 - d1*d6
-        if (vb <= 0.0 .AND. d2 >= 0.0 .AND. d6 <=0.0) then
+        if (vb <= zero .AND. d2 >= zero .AND. d6 <=zero) then
             w = d2 / (d2 - d6)
             closepoint = a + w * ac ! barycentric (1-w, 0, w)
             diff = closepoint - p
@@ -80,7 +84,7 @@ module triangles
 
         ! Check if P in edge region of BC, if so, return proj(P,BC)
         va = d3*d6 - d5*d4
-        if (va <= 0.0 .AND. (d4-d3) >= 0.0 .AND. (d5-d6) >= 0.0) then
+        if (va <= zero .AND. (d4-d3) >= zero .AND. (d5-d6) >= zero) then
             w = (d4 - d3) / ((d4 - d3) + (d5 - d6))
             closepoint = b + w * (c - b) ! barycentric (0, 1-w, w)
             diff = closepoint - p
@@ -90,7 +94,7 @@ module triangles
         end if
 
         ! P inside face region. Compute Q through barycentric (u, v, w)
-        denom = 1.0 / (va + vb + vc)
+        denom = one / (va + vb + vc)
         v = vb * denom
         w = vc * denom
         closepoint = a + ab * v + ac * w
@@ -103,8 +107,8 @@ module triangles
 
     subroutine clamp(n, min, max)
         implicit none
-        real, intent(in) :: min, max
-        real, intent(inout) :: n
+        real(kind=realType), intent(in) :: min, max
+        real(kind=realType), intent(inout) :: n
 
         if (n < min) then
             n = min
@@ -116,11 +120,11 @@ module triangles
 
     subroutine line_line(p1, q1, p2, q2, dsquared)
         implicit none
-        real, dimension(3), intent(in) :: p1, q1, p2, q2
-        real, intent(out) :: dsquared
-        real, dimension(3) :: d1, d2, r, diff, c1, c2
-        real, parameter :: EPS = 1e-12
-        real :: a, b, c, e, f, s, t, denom
+        real(kind=realType), dimension(3), intent(in) :: p1, q1, p2, q2
+        real(kind=realType), intent(out) :: dsquared
+        real(kind=realType), dimension(3) :: d1, d2, r, diff, c1, c2
+        real(kind=realType), parameter :: EPS = 1e-12
+        real(kind=realType) :: a, b, c, e, f, s, t, denom
 
         d1 = q1 - p1
         d2 = q2 - p2
@@ -136,35 +140,35 @@ module triangles
             return
         end if
         if (a <= EPS) then
-            s = 0.0
+            s = zero
             t = f / e
-            call clamp(t, 0.0, 1.0)
+            call clamp(t, zero, one)
         else
             call dot_prod(c, d1, r)
             if (e <= EPS) then
-                t = 0.0
+                t = zero
                 s = -c / a
-                call clamp(s, 0.0, 1.0)
+                call clamp(s, zero, one)
             else
                 ! General non-degenerate case
                 call dot_prod(b, d1, d2)
                 denom = a*e - b*b
-                if (denom /= 0.0) then
+                if (denom /= zero) then
                     s = (b*f - c*e) / denom
-                    call clamp(s, 0.0, 1.0)
+                    call clamp(s, zero, one)
                 else
-                    s = 0.0
+                    s = zero
                 end if
 
                 t = (b*s + f)/e
-                if (t < 0.0) then
-                    t = 0.0
+                if (t < zero) then
+                    t = zero
                     s = -c / a
-                    call clamp(s, 0.0, 1.0)
-                else if (t > 1.0) then
-                    t = 1.0
+                    call clamp(s, zero, one)
+                else if (t > one) then
+                    t = one
                     s = (b - c)/a
-                    call clamp(s, 0.0, 1.0)
+                    call clamp(s, zero, one)
                 end if
             end if
         end if
