db41c0efa6344c561d489c5e5f8fd5f4ee681cac
Eirikur Jonsson <36180221+eirikurj@users.noreply.github.com>
2022-09-21 14:07:17 +0000

    Updating compiler flags (#6)

--- config/defaults/config.LINUX_GFORTRAN.mk
+++ config/defaults/config.LINUX_GFORTRAN.mk
@@ -1,5 +1,5 @@
-FCOMPILER=mpif90
+FCOMPILER=mpifort
 FCOMPILER_ALL_FLAGS=$(FCOMPILER) -O3
 F2PY_INCLUDES=-I$(MPI_INSTALL_DIR)/include
-F2PY_ALL_FLAGS=--f90exec=$(FCOMPILER) $(F2PY_INCLUDES) --opt='-O3'
-F2PY=python3 -m numpy.f2py 
+F2PY_ALL_FLAGS=--fcompiler=gnu95 --f90exec=$(FCOMPILER) --f77exec=$(FCOMPILER) $(F2PY_INCLUDES) --opt='-O3'
+F2PY=python3 -m numpy.f2py
